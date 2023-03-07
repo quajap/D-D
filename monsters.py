ee8d55076970b52aea4df0d18a0ca3ee98cf5a8b
quajap <127042320+quajap@users.noreply.github.com>
2023-03-07 02:23:31 +0200

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -1,26 +1,32 @@
-# Define a class to represent a D&D character
-class Character:
-    def __init__(self, name, race, class_, strength, dexterity, constitution, intelligence, wisdom, charisma):
+# Define a class to represent a monster
+class Monster:
+    def __init__(self, name, hit_points, armor_class, attack_bonus, damage_dice):
         self.name = name
-        self.race = race
-        self.class_ = class_
-        self.strength = strength
-        self.dexterity = dexterity
-        self.constitution = constitution
-        self.intelligence = intelligence
-        self.wisdom = wisdom
-        self.charisma = charisma
+        self.hit_points = hit_points
+        self.armor_class = armor_class
+        self.attack_bonus = attack_bonus
+        self.damage_dice = damage_dice
 
-    # Define a method to calculate the character's hit points
-    def calculate_hit_points(self):
-        if self.class_ == "Fighter":
-            return 10 + self.constitution
-        elif self.class_ == "Wizard":
-            return 6 + self.constitution
+    # Define a method for the monster to make an attack against a character
+    def attack(self, character):
+        roll = roll_die(20)
+        if roll + self.attack_bonus >= character.armor_class:
+            damage = roll_dice(*self.damage_dice)
+            character.hit_points -= damage
+            print(self.name, "hits", character.name, "for", damage, "points of damage!")
         else:
-            return 8 + self.constitution
+            print(self.name, "misses", character.name)
 
-# Example usage: create a new character and display their hit points
-new_character = Character("Gandalf", "Human", "Wizard", 10, 12, 8, 18, 16, 14)
-hit_points = new_character.calculate_hit_points()
-print(new_character.name, "has", hit_points, "hit points.")
+# Example usage: simulate a combat encounter between a character and a monster
+character = Character("Conan", "Human", "Fighter", 16, 14, 12, 10, 8, 6)
+monster = Monster("Goblin", 7, 13, 2, (1, 6))
+
+while character.hit_points > 0 and monster.hit_points > 0:
+    monster.attack(character)
+    if character.hit_points <= 0:
+        print("The", monster.name, "has defeated", character.name)
+        break
+    character.attack(monster)
+    if monster.hit_points <= 0:
+        print(character.name, "has defeated the", monster.name)
+        break
